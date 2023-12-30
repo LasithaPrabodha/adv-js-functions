017cad7b7789414bd119002e08ba92eeed7f79f1
Lasitha Prabodha <lasiprabo@gmail.com>
2023-12-30 18:02:19 -0500

    Update omit.js

--- src/omit.js
+++ src/omit.js
@@ -8,11 +8,11 @@ function omit(obj, keys) {
 }
 const obj = {
   id: 1,
-  name: "Paul",
+  name: "Lasitha",
   job: "Senior Engineer",
-  twitter: "https://www.twitter.com/paulknulst",
-  website: "https://www.paulsblog.dev",
+  linkedin: "https://www.linkedin.com/in/lasithapw",
+  website: "https://www.lasitha.dev",
 };
 
 const selected = omit(obj, ["id"]);
-console.log(selected); // {name: 'Paul', job: 'Senior Engineer', twitter: 'https://www.twitter.com/paulknulst', website: 'https://www.paulsblog.dev'}
+console.log(selected); // {name: 'Lasitha', job: 'Senior Engineer', linkedin: 'https://www.linkedin.com/in/lasithapw', website: 'https://www.lasitha.dev'}
